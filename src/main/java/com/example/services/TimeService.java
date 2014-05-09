a4b48e90fe38f26857988da67ed5767a57f0a3f1
Lawrence Taylor <ltaylor0430@yahoo.com>
2014-05-08 22:29:50 -0400

    New changes to deploy

--- src/main/java/com/example/services/TimeService.java
+++ src/main/java/com/example/services/TimeService.java
@@ -6,6 +6,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.PathParam;
+import java.util.TimeZone;
 
 @Path("/time")
 @Produces(MediaType.APPLICATION_JSON)
@@ -14,6 +16,12 @@ public class TimeService {
     @GET
     public Time get() {
         return new Time();
+    }
+    @GET
+    @Path("/{timezone}")
+    public Time get(@PathParam("timezone") String timezone) {
+    	return new Time(TimeZone.getTimeZone(timezone.toUpperCase()));
+
     }
 
 }
